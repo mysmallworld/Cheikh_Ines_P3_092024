b59b9e13e64ff7b58c5df537baff8c6b42dcc318
Inès C <ines.parisdescartes@gmail.com>
2024-12-06 09:21:31 +0100

    fix(refacto): add doc swagger

--- estate/src/main/java/com/chatop/estate/configuration/SwaggerConfig.java
+++ estate/src/main/java/com/chatop/estate/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.chatop.estate.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,15 @@ public class SwaggerConfig {
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .info(new Info()
+                        .title("Chatop Estate API")
+                        .description("API to manage Estate announcements, users and authentications.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Support Chatop")
+                                .email("support@chatop.com")
+                                .url("https://www.chatop.com")
+                        ))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName)
                 ).components(new Components()
--- estate/src/main/java/com/chatop/estate/model/Message.java
+++ estate/src/main/java/com/chatop/estate/model/Message.java
@@ -2,8 +2,6 @@ package com.chatop.estate.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
