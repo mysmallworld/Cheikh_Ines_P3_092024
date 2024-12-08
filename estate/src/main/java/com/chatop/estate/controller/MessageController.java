0f175d01432a4ff4a8ec82e2fff7cff16f8badc9
Inès C <ines.parisdescartes@gmail.com>
2024-12-08 22:54:42 +0100

    fix(refacto): add responses and clean swagger

--- estate/src/main/java/com/chatop/estate/controller/MessageController.java
+++ estate/src/main/java/com/chatop/estate/controller/MessageController.java
@@ -1,20 +1,35 @@
 package com.chatop.estate.controller;
 
 import com.chatop.estate.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
 
-    private MessageService messageService;
+    private final MessageService messageService;
 
     public MessageController(MessageService messageService) {
         this.messageService = messageService;
     }
 
+    @Operation(
+            summary = "Send a new message",
+            description = "Allows the user to post a new message associated with a rental or user."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Message send with success"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @PostMapping()
-    public String postMessage(@RequestParam("message") String message){
-        return messageService.postMessage(message);
+    public ResponseEntity<String> postMessage(@RequestParam("message") String message, @RequestHeader("Authorization") String authorizationHeader){
+        messageService.postMessage(message, authorizationHeader);
+        return new ResponseEntity<>("Message send with success", HttpStatus.OK);
     }
 }
--- estate/src/main/java/com/chatop/estate/model/Message.java
+++ estate/src/main/java/com/chatop/estate/model/Message.java
@@ -1,19 +1,26 @@
 package com.chatop.estate.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name="messages")
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    private UUID id;
 
     @Column(length = 2000)
     private String message;
@@ -33,52 +40,4 @@ public class Message {
     @ManyToOne
     @JoinColumn(name="rental_id")
     private Rental rental;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Rental getRental() {
-        return rental;
-    }
-
-    public void setRental(Rental rental) {
-        this.rental = rental;
-    }
 }
