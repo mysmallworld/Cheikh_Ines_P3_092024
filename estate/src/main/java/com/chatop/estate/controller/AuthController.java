6e5a2934a3f4f95e8eb42f7b8a6f17cb85f78e24
Inès C <ines.parisdescartes@gmail.com>
2024-12-09 20:32:25 +0100

    fix(refacto): add swagger doc

--- estate/src/main/java/com/chatop/estate/controller/AuthController.java
+++ estate/src/main/java/com/chatop/estate/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.chatop.estate.dto.RegisterUserDto;
 import com.chatop.estate.dto.UserResponseDto;
 import com.chatop.estate.service.AuthService;
 import com.chatop.estate.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,16 +23,40 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Register a new user",
+            description = "Allows a user to register."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Token JWT"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @PostMapping("/register")
     public String registerUser(@RequestBody RegisterUserDto userDto){
         return authService.registerUser(userDto);
     }
 
+    @Operation(
+            summary = "Login a user",
+            description = "Allows a user to login."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Token JWT"),
+            @ApiResponse(responseCode = "401", description = "Bad informations")
+    })
     @PostMapping("/login")
     public String loginUser(@RequestBody LoginUserDto userDto) {
         return authService.loginUser(userDto);
     }
 
+    @Operation(
+            summary = "Get a user",
+            description = "Allows a user to get his informations."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User informations"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @GetMapping("/me")
     public UserResponseDto getUser(@RequestHeader("Authorization") String authorizationHeader) {
         return userService.getUser(authorizationHeader);
--- estate/src/main/java/com/chatop/estate/controller/RentalController.java
+++ estate/src/main/java/com/chatop/estate/controller/RentalController.java
@@ -2,6 +2,9 @@ package com.chatop.estate.controller;
 
 import com.chatop.estate.dto.RentalDto;
 import com.chatop.estate.service.RentalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,16 +21,40 @@ public class RentalController {
         this.rentalService = rentalService;
     }
 
+    @Operation(
+            summary = "Get all rentals",
+            description = "Allows a user to get all rentals."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All rentals"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @GetMapping("")
     public List<RentalDto> getAllRentals(){
         return rentalService.getAllRentals();
     }
 
+    @Operation(
+            summary = "Get a rental",
+            description = "Allows a user to get a rental."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "One rental"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @GetMapping("/{id}")
     public RentalDto getRental(@PathVariable UUID id){
         return rentalService.getRental(id);
     }
 
+    @Operation(
+            summary = "Post a rental",
+            description = "Allows a user to post a rental."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rental created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @PostMapping("/{id}")
     public String postRental(@PathVariable("id") UUID id,
                              @RequestParam("name") String name,
@@ -38,6 +65,14 @@ public class RentalController {
         return rentalService.postRental(id, name, surface, price, description, picture);
     }
 
+    @Operation(
+            summary = "Update a rental",
+            description = "Allows a user to update his rental."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rental updated"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @PutMapping("/{id}")
     public String updateRental(@PathVariable("id") UUID id,
                                @RequestPart(value="name", required = false) String name,
